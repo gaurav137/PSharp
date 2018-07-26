67a3a59cf3e9c71f97c9253b955638d203e61d65
Pantazis Deligiannis <pdeligia@microsoft.com>
2018-07-26 13:39:38 +0100

    fix in async task scheduler log

--- Source/TestingServices/Scheduling/AsynchronousTaskScheduler.cs
+++ Source/TestingServices/Scheduling/AsynchronousTaskScheduler.cs
@@ -1,7 +1,7 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright file="AsynchronousTaskScheduler.cs">
 //      Copyright (c) Microsoft Corporation. All rights reserved.
-// 
+//
 //      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 //      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 //      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
